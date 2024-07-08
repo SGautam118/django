b6f7f4eb2984536c8870d0c97b52714a5afaf821
SGautam118 <75456668+SGautam118@users.noreply.github.com>
2024-07-09 01:20:35 +0530

    Update manage.py

--- django_project/manage.py
+++ django_project/manage.py
@@ -2,11 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
 
 
 def main():
-    load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
     try:
         from django.core.management import execute_from_command_line
